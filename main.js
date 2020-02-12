1c28119a145dac21858a2bcc9a0118c931bf48a8
Kylian Mendy <kylianmendy@MacBook-Pro-de-Kylian.local>
2020-02-12 14:32:19 +0100

    anime gallery project with a js library

---
+++ main.js
@@ -0,0 +1,18 @@
+const sr = ScrollReveal();
+
+sr.reveal('h1',{
+  origin: top,
+  duration: 3000,
+  scale:0.5,
+  reset:true,
+});
+
+sr.reveal('.élem1',{
+delay:1000,
+duration: 2000,
+reset:true,
+},300);
+
+
+
+console.log('salut');
